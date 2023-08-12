94d44749d037f35d31f39d94303dbc277eb89bd3
Sherelle <sherellie1@hotmail.com>
2023-08-12 09:29:55 +0100

    pushing correct cars file to github

--- exercises/object-oriented-programming/src/main/java/com/cbfacademy/cars/Showroom.java
+++ exercises/object-oriented-programming/src/main/java/com/cbfacademy/cars/Showroom.java
@@ -5,19 +5,13 @@ package com.cbfacademy.cars;
         public static void main(String[] args) {
             System.out.println("Hello World!");
             Car car1 = new Car("Volvo","V40",2012);
-        Car car2 = new Car("Porsche","Panamera",2009);
-        Car car3 = new Car("Audi","A3",2018);
-        System.out.println(car1.model + " "+ car1.make +" " + car1.year);
-        System.out.println(car2.model + " "+ car2.make +" " + car2.year);
-        System.out.println(car3.model + " "+ car3.make +" " + car3.year);
-
-     String make; 
-     String model; 
-     int year;
-
-    public Showroom(String make, String model, int year){
-        this.make = make;
-        this.model = model;
-        this.year = year;
-    }
+            Car car2 = new Car("Porsche","Panamera",2009);
+            Car car3 = new Car("Audi","A3",2018);
+            car1.getDetals();
+            car2.getDetals();
+            car3.getDetals();
+        
+            System.out.println(car1.toString());
+        }
+    
 }
