c216d381d9df443b2e11bd682523183c051fcd65
Sherelle <sherellie1@hotmail.com>
2023-08-12 09:43:32 +0100

    cars corrected, get details in showroom fixed

--- exercises/object-oriented-programming/src/main/java/com/cbfacademy/cars/Showroom.java
+++ exercises/object-oriented-programming/src/main/java/com/cbfacademy/cars/Showroom.java
@@ -7,9 +7,9 @@ package com.cbfacademy.cars;
             Car car1 = new Car("Volvo","V40",2012);
             Car car2 = new Car("Porsche","Panamera",2009);
             Car car3 = new Car("Audi","A3",2018);
-            car1.getDetals();
-            car2.getDetals();
-            car3.getDetals();
+            System.out.print(car1.getDetals());
+            System.out.print(car2.getDetals());
+            System.out.print(car3.getDetals());
         
             System.out.println(car1.toString());
         }
