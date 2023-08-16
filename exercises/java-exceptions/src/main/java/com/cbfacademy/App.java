a9507b2481e1641d17f890e82fe5380f0a273442
Sherelle <sherellie1@hotmail.com>
2023-08-16 23:42:48 +0100

    setting up

--- exercises/java-exceptions/src/main/java/com/cbfacademy/App.java
+++ exercises/java-exceptions/src/main/java/com/cbfacademy/App.java
@@ -1,13 +1,17 @@
 package com.cbfacademy;
 
+import java.lang.reflect.Array;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ){
+
+        System.out.println("Hello world");
+
+        // Array.asList("App.java", "App.txt", null, "App.md");
     }
 }
---
+++ exercises/java-exceptions/src/main/java/com/cbfacademy/FileExtension.java
@@ -0,0 +1,20 @@
+package com.cbfacademy;
+
+public class FileExtension {
+    
+    public int check(String FileExtension){
+
+        if(FileExtension.contains(".java")){
+            return 1;
+        }else if(!FileExtension.contains(".java")){
+            return 0;
+        }else{
+            
+        }
+    //    public boolean FileExtension.contains(CharSequence ".java"); //get this to return 1
+        // if file extension is .java return 1
+        // if file extension is not java return 0
+        // throw a custom exception when name is  or an empty string 
+
+    }
+}
