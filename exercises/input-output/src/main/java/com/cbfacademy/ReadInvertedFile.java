137b251e3ed6e06a3bccf847afb4fc0730e5876a
Sherelle <sherellie1@hotmail.com>
2023-08-23 15:30:43 +0100

    reverse file using stream has been complete.

--- exercises/input-output/src/main/java/com/cbfacademy/ReadInvertedFile.java
+++ exercises/input-output/src/main/java/com/cbfacademy/ReadInvertedFile.java
@@ -1,13 +1,10 @@
 package com.cbfacademy;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
@@ -16,13 +13,15 @@ import java.util.stream.Stream;
 public class ReadInvertedFile {
     // // create a programme to invert the exercise.txt file
     public List<String> readFileInList(String fileName){
-        List<String> lines = Collections.emptyList();
+        //DELETE  List<String> lines = Collections.emptyList();
         try{
-            lines= Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8);
+            // lines= 
+            return Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8);
         }catch(IOException e){
             System.out.println("Fatal error");
+            return Collections.emptyList();
         }
-        return lines;
+        // DELELE return lines;
     }
     
     public static  <T> Collector<T, ?, Stream<T> > reverseStream(){
@@ -31,16 +30,4 @@ public class ReadInvertedFile {
             return list.stream();
         });
     }
-
-    // public static void OutputStream(String data){
-    //     OutputStream os = null;
-    //     try{
-    //         os = new FileOutputStream(new File("src/main/resources/exercise3.txt"));
-    //         os.write(data.getBytes(), 0 data.length());
-    //     }catch(IOException e){
-    //         e.printStackTrace();
-    //         System.out.println("Could not write to file");
-    //     }
-    // }
-    
 }
