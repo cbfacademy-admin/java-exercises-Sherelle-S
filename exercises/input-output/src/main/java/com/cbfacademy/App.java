bb59583172ac1a8dae2ae59314b38f8974a2fb86
Sherelle <sherellie1@hotmail.com>
2023-08-23 23:28:45 +0100

    commented all the code so far

--- exercises/input-output/src/main/java/com/cbfacademy/App.java
+++ exercises/input-output/src/main/java/com/cbfacademy/App.java
@@ -1,42 +1,26 @@
 package com.cbfacademy;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.LineNumberReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import com.cbfacademy.ReadAFile;
 
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         // Read the file
         String filePath = "src/main/resources/exercise.txt";
-       
+        // String outputPath = "src/main/resources/writingExercise.txt";
 
         
         try {
             ReadAFile readAFile = new ReadAFile();
             readAFile.readingFile(filePath);
+            WritingFile writeFile = new WritingFile();
+            writeFile.readAndWriteFile(filePath);
         } catch (IOException e) {
             e.printStackTrace();
-            System.out.println("Error in Reading file");
-        }
-        // WriteFile writeFile = new WriteFile();
-
-        // filePath.writeFile();
-        
-        
-           
-       
+            System.out.println("Error in Reading");
+        }     
     }
 }
 
     
 
-// put a buffered reader arounf the stream reader to get access to the
--- exercises/input-output/src/main/java/com/cbfacademy/WritingFile.java
+++ exercises/input-output/src/main/java/com/cbfacademy/WritingFile.java
@@ -1,27 +1,56 @@
 package com.cbfacademy;
 
-import java.io.FileOutputStream;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
 
 public class WritingFile {
+    public static final char[] bufferedReader = null;
+    public void readAndWriteFile(String inputPath) throws IOException{
+            //  try(
+            //     //  FileInputStream file = new FileInputStream(filePath);
+            //     //  InputStreamReader streamReader = new InputStreamReader(file);
+            //      BufferedReader bufferedReader = new BufferedReader(streamReader);
+            //      LineNumberReader lineReader = new LineNumberReader(bufferedReader);
+            // ){
+            //     String outputLine = lineReader.readLine();
+            //     while(outputLine != null){
+            //         System.out.printf("%d: %s%n", lineReader.getLineNumber(), outputLine);
+            //         outputLine = lineReader.readLine();
+            //     }
+            try {
+                String outputPath = "src/main/resources/writingExercise.txt";
+                FileReader reader = new FileReader(inputPath);
+                LineNumberReader lineReader = new LineNumberReader(reader);
 
-     public void writingFile(){
-            String fileName = "exercise2.txt";
+                FileWriter writer = new FileWriter(outputPath);
 
-        try(
-            FileOutputStream fileOutputStream = new FileOutputStream(fileName);
-            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream);
-            PrintWriter writer = new PrintWriter(outputStreamWriter);
-        ){
-            writer.print(ReadAFile.bufferedReader);//get access to this buffered reader currently outside of your scope.
-            writer.close();
-            outputStreamWriter.close();
-        }catch (IOException e){
-            System.out.println("File writing exception: " + e.getMessage());
+                String outputLine = lineReader.readLine();
+                
+                        while(outputLine != null){
+                            
+                    String formattedText = String.format("%d: %s%n", lineReader.getLineNumber(), outputLine);
+                    writer.write(formattedText);
+
+                    System.out.print(formattedText);
+                    
+                   outputLine = lineReader.readLine();
+                }
+               
+                reader.close();
+                writer.close();
+            } catch (FileNotFoundException e) {
+                // TODO: handle exception
+                System.out.println("File not found, please check file path");
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+            
+           
         }
-    }
-    
-    
 }
