29fdd1fe5fc8d027c73b8423bc8d0983f163e5e0
Sherelle <sherellie1@hotmail.com>
2023-08-29 16:50:23 +0100

    read/write stream complete

--- exercises/input-output/src/main/java/com/cbfacademy/App.java
+++ exercises/input-output/src/main/java/com/cbfacademy/App.java
@@ -5,48 +5,61 @@ import java.io.PrintWriter;
 import java.util.stream.Stream;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // System.out.println("Hello World!");
         // Read the file
         String filePath = "src/main/resources/exercise.txt";
-        String outputPath = "src/main/resources/writingExercise.txt";
+        String outputPath = "src/main/resources/inverted1.txt";
 
         
-        try {
-            // opens try catch block which can run now thing swill throw error
-            ReadAFile readAFile = new ReadAFile();
-            // new readAFile instance
-            readAFile.readingFile(filePath);
-            // running readingFile method on readAFile instance passing the filePath into it.
-            WritingFile writeFile = new WritingFile();
-            // new writeFile class
-            writeFile.readAndWriteFile(filePath);
+        // try {
+        //     // opens try catch block which can run now thing swill throw error
+        //     ReadAFile readAFile = new ReadAFile();
+        //     // new readAFile instance
+        //     readAFile.readingFile(filePath);
+        //     // running readingFile method on readAFile instance passing the filePath into it.
+        //     WritingFile writeFile = new WritingFile();
+        //     // new writeFile class
+        //     writeFile.readAndWriteFile(filePath);
            
-            // new instan
-        } catch (IOException e) {
-            // throws an IOexception
-            e.printStackTrace();
-            // prints a trace if exception is triggered
-            System.out.println("Error in Reading");
-            // error message
-        }     
+        //     // new instan
+        // } catch (IOException e) {
+        //     // throws an IOexception
+        //     e.printStackTrace();
+        //     // prints a trace if exception is triggered
+        //     System.out.println("Error in Reading");
+        //     // error message
+        // }     
     // }close try-catch block
       ReadInvertedFile invertFile = new ReadInvertedFile();
 
+      invertFile.ReadInvertedFile(filePath, outputPath);
+
     
-        try {
-            Stream<String> invertedStream = invertFile.readFileInList(filePath).stream().collect(ReadInvertedFile.reverseStream());
+        // try {
+        //     Stream<String> invertedStream = invertFile.readFileInList(filePath).stream().collect(ReadInvertedFile.reverseStream());
 
-            // Use try-with-resources to handle the file writing
-            try (PrintWriter writer = new PrintWriter(outputPath)) {
-                invertedStream.forEach(writer::println);
-            }
+        //     // Use try-with-resources to handle the file writing
+        //     try (PrintWriter writer = new PrintWriter(outputPath)) {
+        //         invertedStream.forEach(writer::println);
+        //     }
 
-            System.out.println("Content inverted and written to 'inverted.txt' successfully.");
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("Error occurred while processing the file.");
-        }
+        //     System.out.println("Content inverted and written to 'inverted.txt' successfully.");
+        // } catch (Exception e) {
+        //     e.printStackTrace();
+        //     System.out.println("Error occurred while processing the file.");
+        // }
+
+        // // will be refactoring code to look more like this i think
+        //  ReadInvertedFile invertFile = new ReadInvertedFile();
+      
+
+        // // Use try-with-resources to handle the file writing
+        //     try{
+        //          Stream<String> invertedStream = invertFile.readFileInList(filePath).stream().collect(ReadInvertedFile.reverseStream());
+        //         try (PrintWriter writer = new PrintWriter(outputPath)) {
+        //         invertedStream.forEach(writer::println);
+        //     }
     }
     // closes main method
 }
--- exercises/input-output/src/main/java/com/cbfacademy/ReadInvertedFile.java
+++ exercises/input-output/src/main/java/com/cbfacademy/ReadInvertedFile.java
@@ -1,9 +1,12 @@
 package com.cbfacademy;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collector;
@@ -12,22 +15,38 @@ import java.util.stream.Stream;
 
 public class ReadInvertedFile {
     // // create a programme to invert the exercise.txt file
-    public List<String> readFileInList(String fileName){
-        //DELETE  List<String> lines = Collections.emptyList();
-        try{
-            // lines= 
-            return Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8);
-        }catch(IOException e){
-            System.out.println("Fatal error");
-            return Collections.emptyList();
-        }
-        // DELELE return lines;
+    public void ReadInvertedFile (String fileName, String outputPath) throws IOException{
+        List <String> textLine =  Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8);
+        Collections.reverse(textLine);
+
+        List<String> reversedLines = textLine.stream().collect(Collectors.toList());
+
+       Files.write(Paths.get(outputPath), reversedLines);
+        System.out.println(reversedLines);
     }
-    
-    public static  <T> Collector<T, ?, Stream<T> > reverseStream(){
-        return Collectors.collectingAndThen(Collectors.toList(), list -> {
-            Collections.reverse(list);
-            return list.stream();
-        });
-    }
+    // return textLine;
+//    public List<String> readFileInList(String fileName){
+//     // creates a list of strings called readFileList passing in fileName which is the input string.
+//         try{
+//             // opens trycatch block
+//             return Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8);
+//             /* File - static method that allows file to be manipulated in this case it grants access to:
+//             * reads all lines in the file 
+//             * which we get from the path stored in the variable filename.
+//             *standardCharset in eight-bit UCS transformation format. (extends ASCII character set to use 8-bit code points)
+//             */ 
+
+//         }catch(IOException e){
+//             System.out.println("Fatal error");
+//             return Collections.emptyList();
+//         }
+//         // DELELE return lines;
+//     }
+//     // for loop with control flows look into the reverse method that collections have
+//     public static  <T> Collector<T, ?, Stream<T> > reverseStream(){
+//         return Collectors.collectingAndThen(Collectors.toList(), list -> {
+//             Collections.reverse(list);
+//             return list.stream();
+//         });
+//     // } 
 }
