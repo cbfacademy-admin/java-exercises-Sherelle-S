c7de7892833ffac97861a8aea9e48700c4b79042
Sherelle <sherellie1@hotmail.com>
2023-09-13 23:07:51 +0100

    wrking throuh changes

--- exercises/object-oriented-programming/src/main/java/com/cbfacademy/App.java
+++ exercises/object-oriented-programming/src/main/java/com/cbfacademy/App.java
@@ -1,14 +1,17 @@
 package com.cbfacademy;
-
-import com.cbfacademy.cars.Car;
-import com.cbfacademy.accounts.Account;
-import com.cbfacademy.accounts.CurrentAccount;
-import com.cbfacademy.accounts.SavingsAccount;
+import com.cbfacademy.people.ExtraPerson;
+import com.cbfacademy.people.ImmutablePerson;
 
 
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        
+        ImmutablePerson person = new ImmutablePerson("Jane","Richards");
+        System.out.println(person);
+
+        ExtraPerson missExtra = new ExtraPerson("Beyonce", "Giselle", "Knowles-Carter", "Queen-B");
+        System.out.println(missExtra);
 
     }
 }
